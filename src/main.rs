8f412c54cf0722c8b7e5650794f7ffd6935203cd
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-05-05 23:40:01 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{stdin, stdout, Write};
-use grass_latte::SendTypes;
+use grass_latte::{serve_webpage, SendTypes};
 
 fn main() {
+    serve_webpage();
+    
     for i in 0..100 {
         grass_latte::send(SendTypes::Charlie(format!("{}", i)));
     }
