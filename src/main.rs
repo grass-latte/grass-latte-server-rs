78b7656094cca2c89f8dba67ce9192cdc7e2bd2f
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-05-07 22:54:54 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
 use grass_latte::{serve_webpage, set_port_range};
 use std::io::{stdin, stdout, Write};
+use clap::Parser;
+use derive_getters::Getters;
+
+#[derive(Parser, Debug, Getters)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long, action, help = "Port range for the server to listen on e.g. 3030-3040")]
+    port_range: Option<String>,
+}
+
+fn parse_range(input: &str) -> Result<(u16, u16), String> {
+    if let Some((start_str, end_str)) = input.split_once('-') {
+        let start = start_str.trim().parse::<u16>().map_err(|e| e.to_string())?;
+        let end = end_str.trim().parse::<u16>().map_err(|e| e.to_string())?;
+        if end < start {
+            return Err("End of port range cannot be less than start".to_string());
+        }
+        Ok((start, end))
+    } else {
+        let num = input.trim().parse::<u16>().map_err(|e| e.to_string())?;
+        Ok((num, num))
+    }
+}
 
 fn main() {
-    set_port_range((3030, 3030));
+    let args = Args::parse();
+
+    if let Some(port_range) = args.port_range() {
+        let range = match parse_range(port_range) {
+            Ok(r) => r,
+            Err(e) => {
+                println!("{e}");
+                return;
+            }
+        };
+        println!("Server will listen on ports: {}-{}", range.0, range.1);
+        set_port_range(range);
+    }
+    else {
+        println!("Server will listen on default ports");
+    }
+
     serve_webpage();
 
-    grass_latte::send_node(["Alpha", "Bravo", "Charlie"]);
-    grass_latte::send_text(["Alpha", "Bravo", "Xray"], "xray: 5");
-    grass_latte::send_node(["Alpha", "Bravo", "Delta"]);
-    grass_latte::send_text(["Alpha", "Bravo", "Hotel"], "hotel: ");
-    grass_latte::send_text(["Alpha", "Bravo", "Hotel", "Whiskey"], "whiskey");
-
-    println!("Press enter to exit");
+    println!("Press enter to exit server");
     stdout().flush().ok();
     stdin().read_line(&mut String::new()).ok();
 }
