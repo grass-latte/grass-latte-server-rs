83be08a17d74bbf271d62698a0f94f9176ea1df7
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-05-07 22:38:47 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use std::io::{stdin, stdout, Write};
-use std::thread;
-use std::time::Duration;
 use grass_latte::{serve_webpage, set_port_range};
+use std::io::{stdin, stdout, Write};
 
 fn main() {
     set_port_range((3030, 3030));
     serve_webpage();
 
-    grass_latte::send_node(["a", "b", "c"]);
-    grass_latte::send_node(["a", "d"]);
-
-    thread::sleep(Duration::from_millis(5000));
-    grass_latte::delete_element(["a", "b", "c"]);
+    grass_latte::send_node(["Alpha", "Bravo", "Charlie"]);
+    grass_latte::send_text(["Alpha", "Bravo", "Xray"], "xray: 5");
+    grass_latte::send_node(["Alpha", "Bravo", "Delta"]);
+    grass_latte::send_text(["Alpha", "Bravo", "Hotel"], "hotel: ");
+    grass_latte::send_text(["Alpha", "Bravo", "Hotel", "Whiskey"], "whiskey");
 
     println!("Press enter to exit");
     stdout().flush().ok();
